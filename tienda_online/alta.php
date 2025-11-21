9af7554203f05e47746525f9e7a280c7a058c713
isma4pucela <isma4mb@gmail.com>
2025-11-21 11:02:49 +0100

    .

--- tienda_online/alta.php
+++ tienda_online/alta.php
@@ -1,6 +1,41 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es"> 
+   <head>  
+      <title>Dar de alta usuario</title> 
+      <meta http-equiv="Content-Type" content="text/html; 
+	charset=iso-8859-1" />  
+   </head>  
+<body>  
 <?php  
     include_once "conexion.php";
-
-    $mysqli->query()
-        echo "Has sido dado de alta. <br>"
-?>  
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = $_POST['nombre'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $mysqli->query("INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre','$email','$password')");  
+        
+        echo "Ha sido dado de alta <br>";
+        
+        $mysqli->close(); 
+        echo 'Desconexión realizada.';
+    }
+    else {
+?>
+        <form method="POST" action="">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" required><br><br>
+            
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required><br><br>
+            
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" id="password" required><br><br>
+            
+            <button type="submit">Dar de alta</button>
+        </form>
+<?php
+    }
+?>
+</body>  
+</html>
