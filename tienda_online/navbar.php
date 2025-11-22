0fb0333c39787e6b3da663e5099533e06e6c93b7
isma4_pucela <isma4mb@gmail.com>
2025-11-22 13:58:22 +0100

    .

--- tienda_online/navbar.php
+++ tienda_online/navbar.php
@@ -1,14 +1,14 @@
 <nav class="navbar">
-        <div class="container">
-            <div class="navbar-brand">
-                <a href="index.php" class="logo">Tienda Oficial CD Rioseco</a>
-                <img src="imagenes/rioseco.png" alt="Escudo CD Rioseco" class="logo-imagen" height="65px" width="65px">
-            </div>
-            <input type="checkbox" id="menu-toggle" class="menu-toggle">
-            <ul class="nav-menu">
-                <li><a href="index.php" class="nav-link active">Inicio</a></li>
-                <li><a href="login.php" class="nav-link">Iniciar Sesión</a></li>
-                <li><a href="alta.php" class="nav-link btn-registro">Registrarse</a></li>
-            </ul>
+    <div class="container">
+        <div class="navbar-brand">
+            <a href="index.php" class="logo">Tienda Oficial CD Rioseco</a>
+            <img src="imagenes/rioseco.png" alt="Escudo CD Rioseco" class="logo-imagen" height="65px" width="65px">
         </div>
-    </nav>
+        <input type="checkbox" id="menu-toggle" class="menu-toggle">
+        <ul class="nav-menu">
+            <li><a href="index.php" class="nav-link active">Inicio</a></li>
+            <li><a href="login.php" class="nav-link">Iniciar Sesión</a></li>
+            <li><a href="alta.php" class="nav-link btn-registro">Registrarse</a></li>
+        </ul>
+    </div>
+</nav>
