2775b32401c606e67b0b066adb9a20bb82e10977
isma4_pucela <isma4mb@gmail.com>
2025-11-25 18:30:58 +0100

    .

--- tienda_online/conexion.php
+++ tienda_online/conexion.php
@@ -1,7 +1,6 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "isma");
-    if ($mysqli->connect_errno) {
-        echo "Error de conexión";
-        exit();
-    }
+$mysqli = new mysqli("localhost", "root", "", "isma");
+if ($mysqli->connect_errno) {
+    echo "Error de conexión: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 ?>
