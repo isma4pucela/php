4060d7a1a5f23fb58de98138636750b3fd96b6b0
isma4_pucela <isma4mb@gmail.com>
2026-01-12 16:34:01 +0100

    .

--- tienda_online/tablas.sql
+++ tienda_online/tablas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    contraseña VARCHAR(100) NOT NULL
+    contrasena VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE productos (
@@ -13,11 +13,11 @@ CREATE TABLE ventas (
     id_venta INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,   
     id_producto INT NOT NULL,  
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON UPDATE RESTRICT ON DELETE CASCADE,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
-INSERT INTO productos (id_producto, nombre) 
+INSERT INTO productos (id_producto, nombre, imagen, stock, estado) 
 VALUES (101, '1ª Equipación'), 
        (102, '2ª Equipación'),
        (103, 'Chándal'),
