9af56cfeb4f177241ae0ea6cf67574c72a9d900f
isma4_pucela <isma4mb@gmail.com>
2026-01-15 13:19:03 +0100

    .

--- tienda_online/conexion.php
+++ tienda_online/conexion.php
@@ -1,6 +1,6 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "isma");
-if ($mysqli->connect_errno) {
-    echo "Error de conexión: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+    $mysqli = new mysqli("fdb1032.awardspace.net", "4712043_isma", "ASIR2-isma", "4712043_isma");
+    if ($mysqli->connect_errno) {
+        echo "Error de conexión: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
 ?>
